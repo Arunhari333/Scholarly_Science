0298c4106f6ed3afa4725b8ab296c0d6161dd9e3
Yogesh sharma <42976285+yogesh1199@users.noreply.github.com>
2020-06-26 22:24:31 +0530

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -19,6 +19,44 @@ class UserProfile(models.Model):
     Major = models.CharField(max_length=100, default='')
     StartDate = models.DateField()
     EndDate = models.DateField()
+ 
+    # deatail page
+    accounting = models.BooleanField()
+    consulting = models.BooleanField(default=True)
+    creativeDesign = models.BooleanField(default=True)
+    Engineering = models.BooleanField()
+    finance = models.BooleanField()
+    legal = models.BooleanField(default=True)
+    marketing = models.BooleanField() 
+    nursing = models.BooleanField()
+    operations = models.BooleanField()
+    research = models.BooleanField()
+    SalesDevlopment = models.BooleanField()
+    SoftwereEngg = models.BooleanField()
+    
+    # preffered roles
+    PrefferRoles= models.CharField(max_length=100)
+    # 10 skills
+    skills=models.CharField(max_length=100)
 
+    # radio filed
+
+    proffessional= models.ChoiceField(choices=GENDER_CHOICES, widget=forms.RadioSelect())
+    fresher=models.ChoiceField(choices=GENDER_CHOICES, widget=forms.RadioSelect())
+    student=models.ChoiceField(choices=GENDER_CHOICES, widget=forms.RadioSelect())
+
+    # on the web
+    linkdin = models.CharField(max_length=30)
+    github=models.CharField(max_length=30)
+    weChat= models.CharField(max_length=30)
+    lineID=models.CharField(max_length=30)
+    Dribble=models.CharField(max_length=30)
+    portfolio=models.CharField(max_length=30)
+   
+    # share profile
+    Sharecom= models.BooleanField()
+    # opportunity
+    opportunity=models.BooleanField()
+    
     def __str__(self):
         return '%s' % (self.user)
