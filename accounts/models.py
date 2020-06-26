ba96adf733b71f55d6e86bbc5e9e06567c1ebd3b
Arun <arunhari3210@gmail.com>
2020-06-26 20:30:59 +0530

    Model Registered

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        models.CASCADE(),
+        blank=True,
+        null=True,
+    )
+    FirstName = models.CharField(max_length=100, default='')
+    LastName = models.CharField(max_length=100, default='')
+    Email = models.EmailField(blank=False)
+    Mobile = models.IntegerField(default=0)
+    City = models.CharField(max_length=100, default='')
+    College = models.CharField(max_length=100, default='')
+    Degree = models.CharField(max_length=100, default='')
+    Major = models.CharField(max_length=100, default='')
+    StartDate = models.DateField()
+    EndDate = models.DateField()
+
+    def __str__(self):
+        return '%s' % (self.user)
