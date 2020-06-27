fac473138a0ce49160d5868bb17f137e53b1f196
Yogesh sharma <42976285+yogesh1199@users.noreply.github.com>
2020-06-27 13:34:01 +0530

    Update urls.py

--- Scholarly_Science/urls.py
+++ Scholarly_Science/urls.py
@@ -20,5 +20,5 @@ from .import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name="index"),
-    path('', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
