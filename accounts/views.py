10ecfc51f6480ca33e2737ff461a4cb031e62d7b
Yogesh sharma <42976285+yogesh1199@users.noreply.github.com>
2020-06-27 09:56:17 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register(request):
+	return render(request,'register.html')
+
+def detail(request):
+	return render(request,'detail.html')
